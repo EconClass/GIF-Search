1a76198b180615de30683ed205fe15660d6dffe9
EconClass <bizmail327@gmail.com>
2018-09-27 16:48:06 -0700

    Port change

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Initial Conditions
-var express = require('express');
-var exphbs = require('express-handlebars');
-var app = express();
-var http = require('http');
-var giphy = require('giphy-api')();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const http = require('http');
+const giphy = require('giphy-api')();
+const port = process.env.PORT || 3000;
 
 // Routes
 app.get('/', function (req, res) {
-    let searchterm = req.query.term || "funny dogs"
+    const searchterm = req.query.term || "funny dogs"
     giphy.search(searchterm, function (err, response) {
         res.render('home', {gifs: response.data});
     });
@@ -19,6 +19,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+// Listen
 app.listen(port, function () {
   console.log('Gif Search listening on port localhost:3000!');
 });
